d0816d417015f26bc899354339a2b830ea90907a
matheuspalmeida <matheus.almeida053@gmail.com>
2025-06-22 15:25:30 -0300

    Adiciona arquivos iniciais: configuração do banco de dados em memória, servidor Express com rotas para produtores e debug, e função para dividir strings de produtores.

---
+++ src/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const movieService = require('./services/movies.service');
+const producersService = require('./services/producers.service');
+const debugService = require('./services/debug.service');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+
+app.use(express.json());
+app.get('/producers/awards/intervals', async (req, res) => {
+  try {
+    const rows = await producersService.getProducerIntervals();
+    if (!rows.length) {
+      console.log('[LOG]: Retorno vazio');
+      return res.json({ min: [], max: [] });
+    }
+    const minVal = Math.min(...rows.map(i => i.interval));
+    const maxVal = Math.max(...rows.map(i => i.interval));
+    const min = rows.filter(i => i.interval === minVal);
+    const max = rows.filter(i => i.interval === maxVal);
+    res.json({ min, max });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get('/debug/:table', async (req, res) => {
+  try {
+    const rows = await debugService.getTable(req.params.table);
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+async function startServer() {
+  try {
+    console.log('Iniciando servidor');
+    console.log('Criando tabelas');
+    await movieService.createMoviesTable();
+    await producersService.createProducerIntervalsTable();
+
+   
+    await movieService.insertMoviesFromCSV();
+    await producersService.processAndInsertProducerIntervals();
+    
+    app.listen(PORT, () => {
+      console.log('API disponível em http://localhost:' + PORT);
+    });
+  } catch (err) {
+    console.error('[ERRO] Erro ao iniciar o servidor:', err);
+  }
+}
+
+startServer();
---
+++ src/utils/splitproducers.js
@@ -0,0 +1,8 @@
+function splitProducers(producersStr) {
+  return producersStr
+    .split(/,| and /i)
+    .map(p => p.trim())
+    .filter(Boolean);
+}
+
+module.exports = splitProducers;
